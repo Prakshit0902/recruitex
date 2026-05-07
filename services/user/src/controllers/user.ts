c5c4262e8b278c32d9634890d35a5b4cdbd770b4
Prakshit0902 <s.prakshit2004@gmail.com>
2026-05-07 18:01:58 +0530

    user service completed with minor updates required

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -5,6 +5,9 @@ import { eq, sql } from "drizzle-orm";
 import { db } from "@app/db/client";
 import { PgSerial } from "drizzle-orm/pg-core";
 import ErrorHandler from "../utils/errorHandler.js";
+import e from "express";
+import FormData from "form-data";
+import axios from "axios";
 
 export const myProfile = TryCatch(async(req :AuthenticatedRequest,res) => {
     const user = req.user
@@ -75,5 +78,228 @@ export const userProfile = TryCatch(async(req :AuthenticatedRequest,res) => {
             user
         }   
     )
+
+})
+
+export const updateUserProfile = TryCatch(async(req :AuthenticatedRequest,res) => {
+    const user = req.user
+
+    if (!user) {
+        throw new ErrorHandler(401, 'Unauthorized')
+    }
+
+    const {phoneNumber, bio, name} = req.body
+
+    const newPhoneNumber = phoneNumber || user.phoneNumber
+    const newBio = bio || user.bio
+    const newName = name || user.name
+
+    const [updatedUser] = await db
+        .update(users)
+        .set({
+            phoneNumber : newPhoneNumber,
+            bio : newBio,
+            name : newName
+        })
+        .where(eq(users.userId, user.userId))
+        .returning(
+            {
+                userId : users.userId,
+                name : users.name,
+                email : users.email,
+                phoneNumber : users.phoneNumber,
+                bio : users.bio,    
+            }
+        )
+
+
+        res.json(
+            {
+                message : "User profile updated successfully",
+                user : updatedUser
+            }
+        )
+})
+
+export const updateProfilePic = TryCatch(async(req :AuthenticatedRequest,res) => {
+    const user = req.user
+
+    if (!user) {
+        throw new ErrorHandler(401, 'Unauthorized')
+    }
+
+    const file = req.file
+
+    
+
+    if (!file) {
+        throw new ErrorHandler(400, 'No file uploaded')
+    }
+
+    const oldPublicId = user.profilePicPublicId
+    const formData = new FormData()
+
+    formData.append('file', file.buffer, {
+        filename: file.originalname,
+    })
+
+    formData.append('public_id', oldPublicId || '')
+
+    const {data} = await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload`, formData, {
+        headers : {
+            ...formData.getHeaders()
+        }
+    })
+
+    console.log(data);
+    
+
+    const [updatedUser] = await db
+        .update(users)
+        .set({
+            profilePic : data.url,
+            profilePicPublicId : data.public_id
+        })
+        .where(eq(users.userId, user.userId))
+        .returning(
+            {
+                userId : users.userId,
+                name : users.name,
+                profilePic : users.profilePic
+            }
+        )
+
+        res.json(
+            {
+                message : "Profile picture updated successfully",
+                user : updatedUser
+            }
+        )   
+})
+
+export const updateResume = TryCatch(async(req :AuthenticatedRequest,res) => {
+    const user = req.user
+    if (!user) {
+        throw new ErrorHandler(401, 'Unauthorized')
+    }
+
+    const file = req.file
+
+    if (!file) {
+        throw new ErrorHandler(400, 'No file uploaded')
+    }
+
+    const oldPublicId = user.resumePublicId
+
+    const formData = new FormData()
+
+    formData.append('file', file.buffer, {
+        filename: file.originalname,
+    })
+
+    formData.append('public_id', oldPublicId || '')
+
+    const {data} = await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload`, formData, {
+        headers : {
+            ...formData.getHeaders()
+        }
+    })
+
+    const [updatedUser] = await db
+        .update(users)
+        .set({
+            resume : data.url,
+            resumePublicId : data.public_id
+        })
+        .where(eq(users.userId, user.userId))
+        .returning(
+            {
+                userId : users.userId,
+                name : users.name,
+                resume : users.resume
+            }
+        )
+
+        res.json(
+            {
+                message : "Resume updated successfully",
+                user : updatedUser
+            }
+        )
+        
+})
+
+export const updateUserSkills = TryCatch(async(req :AuthenticatedRequest,res) => {
+    const userId = req.user?.userId
+    const {skillName} = req.body
+
+    if (!userId) {
+        throw new ErrorHandler(401, 'Unauthorized')
+    }
+
+    if (typeof skillName !== 'string' || skillName.trim() === '') {
+        throw new ErrorHandler(400, 'Skill name is required')
+    }
+
+    let skillWasAdded = false
+
+
+    await db.transaction(async (tx) => {
+        const userList = await tx
+            .select()
+            .from(users)
+            .where(eq(users.userId, userId))
+        
+        if (userList.length === 0) {
+            throw new ErrorHandler(404, 'User not found')
+        }
+
+        const [skill] = await tx
+            .insert(skills)
+            .values({
+                name : skillName.trim()
+            })
+            .onConflictDoUpdate(
+                {
+                    target : skills.name,
+                    set : {
+                        name : skillName.trim()
+                    }
+                }
+            )
+            .returning(
+                {
+                    skillId : skills.skillId,
+                }
+            )
+
+            const skillId = skill.skillId
+
+            const insertionResult = await tx
+                .insert(userSkills)
+                .values({
+                    userId : userId,
+                    skillId : skillId
+                })
+                .onConflictDoNothing()
+                .returning(
+                    {
+                        userId : userSkills.userId,
+                    }
+                )
+
+            if (insertionResult.length > 0) {
+                skillWasAdded = true
+            }
+    })
+
+
+    res.json(
+        {
+            message : skillWasAdded ? "Skill added successfully" : "Skill already exists for the user"
+        }
+    )
+
+    
 
 })
--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,11 +1,14 @@
 import { Router } from "express"
-import { myProfile, userProfile } from "../controllers/user.js"
+import { myProfile, updateProfilePic, updateResume, updateUserProfile, userProfile } from "../controllers/user.js"
 import { isAuthenticated } from "../middlewares/auth.js"
+import uploadFile from "../middlewares/multer.js"
 
 const router = Router()
 
 router.get('/me', isAuthenticated ,myProfile)
 router.get('/profile/:userId', isAuthenticated, userProfile)
-
+router.put('/update/profile', isAuthenticated, updateUserProfile)
+router.put('/update/pic', isAuthenticated, uploadFile , updateProfilePic)
+router.put('/update/resume', isAuthenticated, uploadFile , updateResume)
 
 export default router
